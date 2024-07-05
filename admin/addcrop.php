fe979400e7f47f65c83484a6d2636bc80941d889
mahendra maddipati <mahendra5maddipati@gmail.com>
2024-07-05 23:25:18 +0530

    Update addcrop.php

--- admin/addcrop.php
+++ admin/addcrop.php
@@ -19,7 +19,7 @@ require_once './core/authenticator.php';
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom fonts for this template-->
+    <!-- Custom fonts for this template -->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 
     <!-- Page level plugin CSS-->
